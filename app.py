3587601491ffd2fa5dd440d1488d77652f1c5b5a
harry-jooyoung-kim <42903712+harry-jooyoung-kim@users.noreply.github.com>
2025-06-03 11:18:19 +0900

    Implement unified navigation and demo

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template('howitworks.html')
+
+@app.route('/pebble')
+def pebble():
+    return render_template('order.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/compose')
+def compose():
+    return render_template('create.html')
+
+@app.route('/library')
+def library():
+    return render_template('recipes.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
+@app.route('/demo', methods=['POST'])
+def demo():
+    data = request.get_json(force=True)
+    # simple stub returning fixed notes regardless of input
+    response = {
+        'top': ['Citrus', 'Bergamot'],
+        'mid': ['Lavender', 'Rosemary'],
+        'base': ['Cedarwood', 'Musk']
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
